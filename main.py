acd31aa98b5a9025a6f019070251bbac020ef36c
Kroko1337 <z&tLDL9B6TXX9Dk^yLhVHyTeztb>
2021-10-04 23:12:15 +0200

    removed useless code

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-if __name__ == '__main__':
-    primeNumberSize = int(input("How mush prime numbers do you want?\n"))
-    print("Generate", primeNumberSize, "prime numbers:\n")
-    primeNumbers = []
-    current = 1
-    while len(primeNumbers) < primeNumberSize:
-        current += 1
-        primeNumber = True
-        factors = 0
-        for x in range(1, current + 1):
-            if current % x == 0:
-                factors += 1
-                if factors > 2:
-                    primeNumber = False
-                    break
-        if primeNumber:
-            print(current)
-            primeNumbers.append(current)
+primeNumberSize = int(input("How mush prime numbers do you want?\n"))
+print("Generate", primeNumberSize, "prime numbers:\n")
+primeNumbers = []
+current = 1
+while len(primeNumbers) < primeNumberSize:
+    current += 1
+    primeNumber = True
+    factors = 0
+    for x in range(1, current + 1):
+        if current % x == 0:
+            factors += 1
+            if factors > 2:
+                primeNumber = False
+                break
+    if primeNumber:
+        print(current)
+        primeNumbers.append(current)
