96e313a0655744945ce03fd5cc1266107a64f9b3
Kroko1337 <z&tLDL9B6TXX9Dk^yLhVHyTeztb>
2022-05-28 02:25:24 +0200

    Added "Absolute Primes"

--- src/main/kotlin/de/krokoyt/primenumber/Main.kt
+++ src/main/kotlin/de/krokoyt/primenumber/Main.kt
@@ -1,9 +1,12 @@
 package de.krokoyt.primenumber
 
+import java.util.Collections
+import kotlin.math.abs
+
 fun main(args: Array<String>) {
-    var primeNumberSize = 10
-    for(x in args.indices) {
-        when(args[x]) {
+    var primeNumberSize = 30
+    for (x in args.indices) {
+        when (args[x]) {
             "-size" -> {
                 primeNumberSize = args[x + 1].toInt()
             }
@@ -11,21 +14,62 @@ fun main(args: Array<String>) {
     }
     var current = 1
     val primeNumbers = arrayListOf<Int>()
+    val absolute = arrayListOf<Int>()
     while (primeNumbers.size < primeNumberSize) {
         current++
         var primeNumber = true
         var factors = 0
-        for (x in 1 ..current + 1)
-            if(current % x == 0) {
+        var absoluteNumber = false
+        for (x in 1..current + 1)
+            if (current % x == 0) {
                 factors++
-                if(factors > 2) {
+                if (factors > 2) {
                     primeNumber = false
                     break
                 }
             }
-        if(primeNumber) {
-            println(current)
+        if (primeNumber) {
+            if (isAbsolute(current)) {
+                absoluteNumber = true
+                absolute.add(current)
+            }
+            println("$current ${if (absoluteNumber) "Abs" else ""}")
             primeNumbers.add(current)
         }
     }
+    if(absolute.isNotEmpty()) {
+        Collections.sort(absolute, Comparator.naturalOrder())
+        println("")
+        println("-- Absolute --")
+        for (abs in absolute) {
+            println(abs)
+        }
+    }
+}
+
+fun isAbsolute(number: Int): Boolean {
+    val string = number.toString()
+    val numbers = ArrayList<Int>()
+    var current = string
+    for (args in 0..string.length) {
+        current = current.substring(1) + current.substring(0, 1)
+        if (current.toInt() != number && !numbers.contains(current.toInt()))
+            numbers.add(current.toInt())
+    }
+    var matches = 0
+    for(number in numbers) {
+        var factors = 0
+        var primeNumber = true
+        for(x in 1 .. number)
+        if (number % x == 0) {
+            factors++
+            if (factors > 2) {
+                primeNumber = false
+                break
+            }
+        }
+        if(primeNumber)
+            matches++
+    }
+    return matches == numbers.size
 }
